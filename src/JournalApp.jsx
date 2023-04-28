325e4b731d8c20f60c00945af586170393879b58
vhtoledo <querynetworking@gmail.com>
2023-04-28 09:44:10 -0300

    configuracion de rutas principales y secundarias

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,8 +1,9 @@
+import { AppRouter } from "./router/AppRouter"
 
 export const JournalApp = () => {
   return (
     <>
-        <h1>Hola</h1>
+      <AppRouter/>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { JournalApp } from './JournalApp.jsx'
-import './styles.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { JournalApp } from "./JournalApp.jsx";
+import "./styles.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <JournalApp />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <JournalApp />
+    </BrowserRouter>
+  </React.StrictMode>
+);
